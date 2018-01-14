e001c43459faf7fdca9f5db1b9eb0dc7285d90f9
psigrai <jongrainger@me.com>
2018-01-14 09:21:48 +0000

    Half way through copying code

--- catches.py
+++ catches.py
@@ -1,4 +1,33 @@
-Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
-[GCC 6.3.0 20170124] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
+from sense_hat import SenseHat
+##Egg Drop###
+##Coded by dan_aldred###
+
+import time
+import random
+sense = SenseHat()
+sense.clear()
+
+global game_over
+global score
+
+game_over = False
+basket_x = 7
+score = 0
+
+'''main pitch measurement'''
+def basket_move(pitch, basket_x):
+    sense.set_pixel(basket_x, 7, [0,0,0])
+    new_x = basket_x
+    if 1 < pitch <179 and basket_x != 0:
+        new_x -= 1
+    elif 359 > pitch >179 and basket_x != 7:
+        new_x += 1
+    return new_x,
+
+"Main game setup"
+def main()
+    global game_over
+
+
+sense.show_message ("I am working", text_colour = [255,255,0]
+                    
